b524892e051a6462a1103c5088b54774a518385b
shubleen <shubleen.truxco@gmail.com>
2026-03-25 13:36:40 +0530

    url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   // apiUrl: 'http://localhost:8080/wms',
-  apiUrl: 'https://raymonde-ungotten-honey.ngrok-free.dev/wms',
+  apiUrl: 'https://framing-journalist-july-essay.trycloudflare.com/wms',
  firebase: {
    apiKey: "AIzaSyD7RyYAkGjS4qOwH35366qYFGLfMKDYLsc",
   authDomain: "workforce-management-1003.firebaseapp.com",
