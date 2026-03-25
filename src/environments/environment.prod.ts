ff6049203efc081afa6067b08c798f1de40d607a
shubleen <shubleen.truxco@gmail.com>
2026-03-25 13:10:14 +0530

    env prod file added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
-  apiUrl: 'http://localhost:8080/wms',
-  // apiUrl: 'https://raymonde-ungotten-honey.ngrok-free.dev/wms',
+  apiUrl: 'https://raymonde-ungotten-honey.ngrok-free.dev/wms',
  firebase: {
    apiKey: "AIzaSyD7RyYAkGjS4qOwH35366qYFGLfMKDYLsc",
   authDomain: "workforce-management-1003.firebaseapp.com",
